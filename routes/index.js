9dc6b189d78d42bba243c583d844ab9565966f26
Sai Prasad Reddy <74254400+saiprasadreddy33@users.noreply.github.com>
2024-07-30 15:09:52 +0530

    Update index 0.1

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import userRoutes from "./userRoutes.js";
 import taskRoutes from "./taskRoutes.js";
+import { uploadAvatar, updateUserProfile } from "../controllers/userController.js";
+import { protectRoute } from "../middlewares/authMiddlewave.js"; 
 
 const router = express.Router();
 
-router.use("/user", userRoutes); //api/user/login
+router.use("/user", userRoutes); 
 router.use("/task", taskRoutes);
+router.put("/update-profile", protectRoute, uploadAvatar, updateUserProfile);
 
 export default router;
